75c5dade72d6e55357b55041b876b81289e57117
Altin Terstena <alt-2020@outlook.com>
2023-05-19 10:53:28 -0400

    fix: remove unused env variables from config

--- utils/config.js
+++ utils/config.js
@@ -1,15 +1,15 @@
 require("dotenv").config() // Variables stored in the .env file
 
-const MONGODB_URI = process.env.NODE_ENV === 'test'
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI
+// const MONGODB_URI = process.env.NODE_ENV === 'test'
+//     ? process.env.TEST_MONGODB_URI
+//     : process.env.MONGODB_URI
 
 const PORT = process.env.PORT
 
-const SECRET = process.env.SECRET
+// const SECRET = process.env.SECRET
 
 module.exports = {
-    MONGODB_URI,
-    PORT,
-    SECRET
+  PORT,
+  // MONGODB_URI,
+  // SECRET
 }
