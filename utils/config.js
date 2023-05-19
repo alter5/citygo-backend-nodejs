cc1a0a4901f7366ba9c0bec591291cf7fd6b5234
Altin Terstena <alt-2020@outlook.com>
2023-05-19 12:10:30 -0400

    add: database env variables to config module

--- utils/config.js
+++ utils/config.js
@@ -6,10 +6,19 @@ require("dotenv").config() // Variables stored in the .env file
 
 const PORT = process.env.PORT
 
+const DATABASE_USER = process.env.DATABASE_USER
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
+const DATABASE_HOST = process.env.DATABASE_HOST
+
+const DATABASE_CONFIG = {
+  USER: DATABASE_USER,
+  PASSWORD: DATABASE_PASSWORD,
+  HOST: DATABASE_HOST
+}
+
 // const SECRET = process.env.SECRET
 
 module.exports = {
   PORT,
-  // MONGODB_URI,
-  // SECRET
+  DATABASE_CONFIG
 }
