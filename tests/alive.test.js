99fc12387ecdf4eaf8c41e09e07ff578d0da9a31
Altin Terstena <alt-2020@outlook.com>
2023-05-19 10:57:13 -0400

    fix: incorrect test file name

---
+++ tests/alive.test.js
@@ -0,0 +1,14 @@
+// TODO: Create test case checking if the json object is returned from cities
+// Use the supertest library
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /api/cities/test', () => {
+  it('should return a JSON object', async () => {
+    const response = await request(app).get('/api/cities/test');
+
+    expect(response.status).toBe(200);
+    expect(response.header['content-type']).toContain('application/json');
+    expect(response.body.result).toEqual("Success");
+  });
+});
