ac21c93ead59564751e868d1c905be1d5a1dfa26
Altin Terstena <alt-2020@outlook.com>
2023-05-23 19:11:57 -0400

    add: db initialization script

--- scripts/initialize.mjs
+++ scripts/initialize.mjs
@@ -6,23 +6,16 @@ const dbConfig = config.DATABASE_CONFIG
 
 const dbClient = new Client(dbConfig)
 
-dbClient
-  .connect()
-  .then(() => console.log('connected'))
-  .catch((err) => console.error('connection error', err.stack))
-await dbClient.connect()
+// Connect to Postgre DB
+try {
+  await dbClient.connect()
+  console.log('connected')
+} catch (err) {
+  console.log('connection error', err.stack)
+}
 
-// Example config from pg documentation
-// const config = {
-//   host: 'my.database-server.com',
-//   port: 5334,
-//   database: 'database-name',
-//   user: 'database-user',
-//   password: 'secretpassword!!',
-// }
-
-const res = await dbClient.query('SELECT $1::text as message', ['Hello world!'])
-console.log(res.rows[0].message) // Hello world!
+const res = await dbClient.query('SELECT $1::text as message', ['Hello dog!'])
+console.log(res) // Hello world!
 await dbClient.end()
 
 
--- utils/config.js
+++ utils/config.js
@@ -6,17 +6,21 @@ require("dotenv").config() // Variables stored in the .env file
 
 const PORT = process.env.PORT
 
+const DATABASE_HOST = process.env.DATABASE_HOST
+const DATABASE_PORT = process.env.DATABASE_PORT
+const DATABASE_NAME = process.env.DATABASE_NAME
 const DATABASE_USER = process.env.DATABASE_USER
 const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
-const DATABASE_HOST = process.env.DATABASE_HOST
 
 const DATABASE_CONFIG = {
-  HOST: DATABASE_HOST,
-  USER: DATABASE_USER,
-  PASSWORD: DATABASE_PASSWORD
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  database: DATABASE_NAME,
+  user: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  sslmode: "prefer",
+  connect_timeout: 10
 }
-
-// const SECRET = process.env.SECRET
 
 module.exports = {
   PORT,
