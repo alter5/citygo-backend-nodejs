4d84bb315fce114b30890515df2f1152f11b3392
Altin Terstena <alt-2020@outlook.com>
2023-06-01 20:37:45 -0400

    add: db client utility

---
+++ utils/dbClient.js
@@ -0,0 +1,10 @@
+const pgPromise = require("pg-promise")
+const config = require("./config.js")
+
+const dbConfig = config.DATABASE_CONFIG
+
+// Configure DB connection
+const pgp = pgPromise({})
+const dbClient = pgp(dbConfig)
+
+module.exports = dbClient
