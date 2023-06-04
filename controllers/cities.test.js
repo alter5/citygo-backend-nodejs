d0bbb1799e4d8f87597ed37f2faaeaf262c958de
Altin Terstena <alt-2020@outlook.com>
2023-06-04 19:45:05 -0400

    add: mock queries module in test case

--- controllers/cities.test.js
+++ controllers/cities.test.js
@@ -1,17 +1,24 @@
 const request = require("supertest")
 const app = require("../app")
+const queries = require("../utils/queries")
+
+jest.mock("../utils/queries")
 
 const run = () => {
   describe("Controller cities.js", () => {
-    it("should get cities which match query string", async () => {
+    it("should return cities from the queries utility module", async () => {
+      const expectedResponse = { result: ["New York", "West New York"] }
+
+      queries.searchForCities.mockResolvedValue(expectedResponse)
+
       const response = await request(app)
         .get("/api/cities/search")
-        .query({ queryString: "New Yo" })
+        .query({ queryString: "Lorem ipsum" })
 
       expect(response.status).toBe(200)
       expect(response.header["content-type"]).toContain("application/json")
       expect(response.body.result).toEqual(
-        expect.arrayContaining(["New York", "West New York"])
+        expect.arrayContaining(expectedResponse.result)
       )
     })
   })
