b11bba78473beada0e2e324f1ddaaf658f9d20ff
Altin Terstena <alt-2020@outlook.com>
2023-07-29 19:56:08 -0400

    add: error endpoint for testing

--- controllers/cities.js
+++ controllers/cities.js
@@ -1,17 +1,20 @@
 const pgPromise = require("pg-promise")
-const config = require("../utils/config")
 const citiesRouter = require("express").Router()
 const dbConfig = require("../utils/dbClient")
 const queries = require("../utils/queries")
-
-citiesRouter.get("/test", async (request, response) => {
-  response.json({ result: "Success" })
-})
 
 citiesRouter.get("/search", async (request, response) => {
   const queryString = request.query.queryString
   const result = await queries.searchForCities(queryString)
   response.json(result)
+})
+
+citiesRouter.get("/test", async (request, response) => {
+  response.json({ result: "Success" })
+})
+
+citiesRouter.get("/error", async (request, response, next) => {
+  throw Error("An error has occurred in the cities controller module")
 })
 
 module.exports = citiesRouter
