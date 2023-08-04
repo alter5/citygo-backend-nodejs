1b00aa5af3efb7bfb680d7305a074aa943216974
Altin Terstena <alt-2020@outlook.com>
2023-08-04 18:21:56 -0400

    add: select all fields from cities table

--- utils/queries.js
+++ utils/queries.js
@@ -1,16 +1,19 @@
 const dbClient = require("./dbClient")
+const logger = require("../utils/logger")
 const config = require("./config")
 
 const searchForCities = async (queryString) => {
   const sql = `
-  SELECT city_name FROM cities
+  SELECT * FROM cities
   WHERE city_name like $1
   `
   const response = {}
   try {
     const _cities = await dbClient.any(sql, [`%${queryString}%`])
+    console.log("🚀 ~ file: queries.js:13 ~ searchForCities ~ _cities:", _cities)
     const cities = _cities.map((row) => row.city_name)
     response.result = cities
+    logger.info("dog", "hi", {dog: {type: "german shepherd"}}, 3)
   } catch (error) {
     response.error = error
   }
