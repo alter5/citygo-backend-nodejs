f02529d9599bb9fe7950ef2adb08b7ca3b7e732e
Altin Terstena <alt-2020@outlook.com>
2023-08-04 18:22:23 -0400

    add: multiple params feature to logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,22 @@
 const info = (...params) => {
-  console.log(...params)
+  const result = getResultString(...params)
+  console.log(result)
 }
 
 const error = (...params) => {
-  console.error(...params)
+  const result = getResultString(...params)
+  console.error(result)
+}
+
+const getResultString = (...params) => {
+  const strings = convertVariablesToStrings([...params])
+  return strings.join("\n")
+}
+
+const convertVariablesToStrings = (arrayOfElements) => {
+  return arrayOfElements.map((elem) =>
+    typeof elem === 'object' ? JSON.stringify(elem, null, 2) : elem
+  )
 }
 
 module.exports = {
