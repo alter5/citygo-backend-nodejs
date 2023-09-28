b26a931712cd9bae070c197cba4b033a23991df2
Altin Terstena <alt-2020@outlook.com>
2023-09-27 23:50:32 -0400

    refactor: convert global setup into a function

--- globalSetup.js
+++ globalSetup.js
@@ -1,6 +1,9 @@
-const createDatabaseScript = require("./scripts/createDatabase")
+const globalSetup = async () => {
+  const createDatabaseScript = require("./scripts/createDatabase")
 
-// TODO: Create the test database, and insert cities into tables
-// Creates the database used for testing. The test_ prefix is used for the db name.
-await createDatabaseScript.run()
+  // TODO: Create the test database, and insert cities into tables
+  // Creates the database used for testing. The test_ prefix is used for the db name.
+  await createDatabaseScript.run()
+}
 
+module.exports = globalSetup
