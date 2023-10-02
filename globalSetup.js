f412249a98dfb4bc0d56d19959bcc730ffd148f1
Altin Terstena <alt-2020@outlook.com>
2023-10-01 21:41:45 -0400

    fix: import typo

--- globalSetup.js
+++ globalSetup.js
@@ -1,6 +1,6 @@
-const globalSetup = async () => {
-  const createDatabaseScript = require("./scripts/createDatabase")
+const createDatabaseScript = require("./scripts/createDatabase")
 
+const globalSetup = async () => {
   // TODO: Create the test database, and insert cities into tables
   // Creates the database used for testing. The test_ prefix is used for the db name.
   await createDatabaseScript.run()
