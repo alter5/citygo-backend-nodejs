7dd9e36dae70b63bdd022a63200983132bf47eb8
Altin Terstena <alt-2020@outlook.com>
2023-10-03 01:20:32 -0400

    refactor: remove unneeded comments

--- utils/queries.js
+++ utils/queries.js
@@ -10,10 +10,8 @@ const searchForCities = async (queryString) => {
   const response = {}
   try {
     const _cities = await dbClient.any(sql, [`%${queryString}%`])
-    console.log("🚀 ~ file: queries.js:13 ~ searchForCities ~ _cities:", _cities)
     const cities = _cities.map((row) => row.city_name)
     response.result = cities
-    logger.info("dog", "hi", {dog: {type: "german shepherd"}}, 3)
   } catch (error) {
     response.error = error
   }
--- utils/queries.spec.js
+++ utils/queries.spec.js
@@ -10,10 +10,6 @@ describe("Helper queries.js", () => {
   it("should get cities which match query string", async () => {
     const response = await queries.searchForCities("New Yo")
     const resultCities = response.result
-    console.log(
-      "🚀 ~ file: queries.test.js:8 ~ it ~ resultCities:",
-      response
-    )
 
     expect(resultCities).toEqual(
       expect.arrayContaining(["West New York", "New York"])
