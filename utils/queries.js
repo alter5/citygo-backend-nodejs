c4e5183fc760a6478e8879d0d9de25e730ad2ccf
Altin Terstena <alt-2020@outlook.com>
2023-10-31 16:57:48 -0400

    format: queries file

--- utils/queries.js
+++ utils/queries.js
@@ -8,15 +8,16 @@ const searchForCities = async (queryString) => {
   WHERE city_name like $1
   ORDER BY population desc
   `
+
   const response = {}
+  
   try {
     const cities = await dbClient.any(sql, [`%${queryString}%`])
-    // const cities = _cities.map((row) => row.city_name)
-    console.log("🚀 ~ file: queries.js:14 ~ searchForCities ~ _cities:", cities)
     response.result = cities
   } catch (error) {
     response.error = error
   }
+
   return response
 }
 
