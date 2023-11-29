f18762989eaaa01687fcc8869aa03db4c887f885
Altin Terstena <alt-2020@outlook.com>
2023-11-29 17:18:55 -0500

    save

--- utils/queries.js
+++ utils/queries.js
@@ -3,6 +3,32 @@ const logger = require("../utils/logger")
 const config = require("./config")
 
 const searchForCities = async (queryString) => {
+  const sql = /* SQL */ `
+  SELECT * FROM cities
+  WHERE city_name ILIKE '%$1:value%'
+  ORDER BY population desc
+  `
+
+  const response = {}
+
+  try {
+    const cities = await dbClient.any(sql, [queryString])
+    response.result = cities
+  } catch (error) {
+    response.error = error
+  }
+
+  return response
+}
+
+const getMostPopulousCities = asnyc () => {
+  const sql = /* SQL */ `
+  hi
+  `
+  return null
+}
+
+const searchForCities1 = async (queryString) => {
   const sql = `
   SELECT * FROM cities
   WHERE city_name like $1
