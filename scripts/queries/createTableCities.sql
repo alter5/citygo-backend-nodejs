1c368d909a38e17e32bd02b4d80c5164c98a78f0
Altin Terstena <alt-2020@outlook.com>
2024-01-29 00:52:13 -0500

    add: state abbreviation field

--- scripts/queries/createTableCities.sql
+++ scripts/queries/createTableCities.sql
@@ -2,6 +2,7 @@ CREATE TABLE cities(
   id serial not null primary key,
   city_name varchar(50),
   state varchar(50),
+  state_abbreviation varchar(20),
   population integer,
   latitude numeric(10, 6),
   longitude numeric(10, 6)
