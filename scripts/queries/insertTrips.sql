9eb2aacde10f4c229aa04e6754897783a8fb23a4
Altin Terstena <alt-2020@outlook.com>
2024-04-02 17:50:38 -0400

    add: script to insert starting trips

---
+++ scripts/queries/insertTrips.sql
@@ -0,0 +1,60 @@
+INSERT INTO public.trips (
+    id,
+    city_id,
+    title,
+    destinations,
+    description,
+    price_range,
+    duration,
+    created_at
+  )
+VALUES (
+    1,
+    891,
+    'New York in One Day',
+    '[{"name": "Times Square", "address": "Manhattan, NY 10036, USA", "purpose": "Historical Landmark", "imageUrl": "https://i.ytimg.com/vi/tFIN8Smt8_w/maxresdefault.jpg", "location": {"lat": 40.7579747, "lng": -73.9855426}}, {"name": "The Metropolitan Museum of Art", "address": "20 W 34th St., New York, NY 10001, USA", "purpose": "Museum", "imageUrl": "https://th.bing.com/th/id/R.3b54d1f796bfea6e73e5ce26158b7d4e?rik=QYuinIjOgTSXpw&riu=http%3a%2f%2fwww.nycgo.com%2fimages%2fvenues%2f188%2fmetmuseum_marleywhite_5113__x_large.jpg&ehk=NAxSphylfB6b9BKZ9Ymt1mK4A6cRU33z5ChdNnzkiJA%3d&risl=&pid=ImgRaw&r=0", "location": {"lat": 40.7484405, "lng": -73.98566439999999}}, {"name": "Le Crocodile", "address": "80 Wythe Ave, Brooklyn, NY 11249", "purpose": "Restaurant", "imageUrl": "https://i.pinimg.com/originals/51/e2/bb/51e2bb784da5477c24a399a878835fe9.png", "location": {"lat": 40.71305, "lng": -74.00723}}, {"name": "Statue of Liberty", "address": "New York, NY 10004, USA", "purpose": "Historical Landmark", "imageUrl": "https://www.wallpaperflare.com/static/855/993/116/statue-of-liberty-clouds-liberty-enlightening-the-world-torch-wallpaper.jpg", "location": {"lat": 40.6892494, "lng": -74.04450039999999}}]',
+    'Embark on a whirlwind adventure through the bustling streets of New York City, where iconic landmarks such as the Statue of Liberty and Times Square await exploration. Immerse yourself in the city''s vibrant culture, diverse cuisine, and endless entertainment options, making every moment an unforgettable experience. ',
+    4,
+    2,
+    '2024-03-23 02:02:21.633178-04'
+  );
+INSERT INTO public.trips (
+    id,
+    city_id,
+    title,
+    destinations,
+    description,
+    price_range,
+    duration,
+    created_at
+  )
+VALUES (
+    2,
+    595,
+    'Chicago''s Magnificent Mile',
+    '[{"name": "The Art Institute of Chicago", "address": "111 S Michigan Ave, Chicago, IL 60603, USA", "purpose": "Museum", "imageUrl": "https://www.tripsavvy.com/thmb/IzCuPzRtNHwDn17H4LqfTrOBKJY=/2121x1414/filters:fill(auto,1)/chicago-river-tourboat-downtown-chicago-skyscrapers-1139456318-b5e51582061b4c03a2ede6616ccf32da.jpg", "location": {"lat": 41.8795845, "lng": -87.62371329999999}}, {"name": "Millennium Park", "address": "Chicago, IL 60602, USA", "purpose": "Park", "imageUrl": "https://theivyasia.com/wp-content/uploads/2022/07/TIA_StPauls_uncropped_hires_12-min-scaled.jpg", "location": {"lat": 41.8825524, "lng": -87.62255139999999}}, {"name": "Riverwalk", "address": "22 E Wacker Dr, Chicago, IL 60601, USA", "purpose": "Tourist Attraction", "imageUrl": "https://theitalianguest.com/wp-content/uploads/2017/08/johnell-pannell-1600587-unsplash-e1562745835473.jpg", "location": {"lat": 41.8884657, "lng": -87.6215612}}]',
+    'Embark on a whirlwind adventure through the bustling streets of New York City, where iconic landmarks such as the Statue of Liberty and Times Square await exploration. Immerse yourself in the city''s vibrant culture, diverse cuisine, and endless entertainment options, making every moment an unforgettable experience. ',
+    3,
+    4,
+    '2024-03-22 15:58:31.717214-04'
+  );
+INSERT INTO public.trips (
+    id,
+    city_id,
+    title,
+    destinations,
+    description,
+    price_range,
+    duration,
+    created_at
+  )
+VALUES (
+    3,
+    713,
+    'Las Vegas Tour',
+    '[{"name": "Grand Canal Shoppes at The Venetian Resort Las Vegas", "address": "3377 Las Vegas Blvd S Suite 2600, Las Vegas, NV 89109, USA", "purpose": "Shopping Mall", "imageUrl": "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/18/74/24.jpg", "location": {"lat": 36.1223898, "lng": -115.1674242}}, {"name": "Eiffel Tower Restaurant", "address": "3655 S Las Vegas Blvd, Las Vegas, NV 89109, USA", "purpose": "French Restaurant", "imageUrl": "https://assets.simpleviewcms.com/simpleview/image/fetch/c_fill,q_100,w_1200/https://lasvegas.simpleviewcrm.com/images/listings/original_Eiffel-Tower-restaurant_Night-Time-10-8bb6a382bdf25ec.jpg", "location": {"lat": 36.1124662, "lng": -115.17219159999999}}, {"name": "Valley of Fire State Park", "address": "Moapa Valley, NV 89040, USA", "purpose": "Park", "imageUrl": "https://s27363.pcdn.co/wp-content/uploads/2021/03/Arch-in-Valley-of-Fire-1129x754.jpg.optimal.jpg", "location": {"lat": 36.4756593, "lng": -114.5383925}}]',
+    'Embark on a whirlwind adventure through the bustling streets of New York City, where iconic landmarks such as the Statue of Liberty and Times Square await exploration. Immerse yourself in the city''s vibrant culture, diverse cuisine, and endless entertainment options, making every moment an unforgettable experience. ',
+    5,
+    2,
+    '2024-03-22 15:57:58.916974-04'
+  );
